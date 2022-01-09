b27e368a386f860d21da6cf7e74f11a770617d40
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2022-01-09 21:46:22 +0900

    非同期でお天気APIにアクセス

--- app/src/main/java/com/beit_and_pear/android/asyncsample/MainActivity.java
+++ app/src/main/java/com/beit_and_pear/android/asyncsample/MainActivity.java
@@ -1,16 +1,32 @@
 package com.beit_and_pear.android.asyncsample;
 
+import androidx.annotation.UiThread;
+import androidx.annotation.WorkerThread;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.os.HandlerCompat;
 
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -73,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // お天気情報の取得処理を行うメソッド
+    @UiThread
     private void receiveWeatherInfo(final String urlFull) {
         // ここに非同期で天気情報を取得する処理を記述
         Looper mainLooper = Looper.getMainLooper();
@@ -95,20 +112,123 @@ public class MainActivity extends AppCompatActivity {
             _handler = handler;
             _urlFull = urlFull;
         }
+        @WorkerThread
         @Override
         public void run() {
             // ここにWeb APIにアクセスするコードを記述
-            WeatherInfoPostExecutor postExecutor = new WeatherInfoPostExecutor();
+            // HTTP接続を行うHttpURLConnectionオブジェクトを宣言。finallyで解放するためにtryがいで宣言
+            HttpURLConnection con = null;
+            // HTTP接続のレスポンスデータとして取得するInputStreamオブジェクトを宣言。同じくtry外で宣言
+            InputStream is = null;
+            // 天気情報サービスから取得したJSON文字列。天気情報が格納されている
+            String result = "";
+            try {
+                // URLオブジェクトを生成
+                URL url = new URL(_urlFull);
+                // URLオブジェクトからHttpURLConnectionオブジェクトを取得
+                con = (HttpURLConnection) url.openConnection();
+                // 接続に使っても良い時間を設定
+                con.setConnectTimeout(1000);
+                // データ取得に使っても良い時間
+                con.setReadTimeout(1000);
+                // HTTP接続メソッドをGETに設定
+                con.setRequestMethod("GET");
+                // 接続
+                con.connect();
+                // HttpURLConnectionオブジェクトからレスポンスデータを取得
+                is = con.getInputStream();
+                // レスポンスデータであるInputStreamオブジェクトを文字列に変換
+                result = is2String(is);
+            }catch (MalformedURLException ex) {
+                Log.e(DEBUC_TAG, "URL変換失敗", ex);
+            }catch (SocketTimeoutException ex) {
+                Log.w(DEBUC_TAG, "通信タイムアウト", ex);
+            }catch (IOException ex) {
+                Log.e(DEBUC_TAG, "通信失敗", ex);
+            }finally {
+                // HttpURLConnectionオブジェクトがnull出ないなら解放
+                if (con != null) {
+                    con.disconnect();
+                }
+                // InputStreamオブジェクトがnullでないなら解放
+                if (is != null) {
+                    try {
+                        is.close();
+                    }catch (IOException ex) {
+                        Log.e(DEBUC_TAG, "InputStream解放", ex);
+                    }
+                }
+            }
+
+            WeatherInfoPostExecutor postExecutor = new WeatherInfoPostExecutor(result);
             _handler.post(postExecutor);
+        }
+
+        private String is2String(InputStream is) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            StringBuffer sb = new StringBuffer();
+            char[] b = new char[1024];
+            int line;
+            while (0 <= (line = reader.read(b))) {
+                sb.append(b, 0, line);
+            }
+            return sb.toString();
         }
     }
 
     // 非同期でお天気情報を取得した後、UIスレッドでその情報を表示するためのクラス
+    @UiThread
     private class WeatherInfoPostExecutor implements Runnable {
+
+        // 取得したお天気情報JSON文字列
+        private final String _result;
+        // コンストラクタ
+        public WeatherInfoPostExecutor(String result) {
+            _result = result;
+        }
 
         @Override
         public void run() {
             // ここにUIスレッドで行う処理コードを記述
+            // 都市名
+            String cityName = "";
+            // 天気
+            String weather = "";
+            // 緯度
+            String latitude = "";
+            // 軽度
+            String longitude = "";
+            try {
+                // ルートJSONオブジェクトを生成
+                JSONObject rootJSON = new JSONObject(_result);
+                // 都市名文字列を取得
+                cityName = rootJSON.getString("name");
+                // 緯度軽度情報JSONオブジェクトを取得
+                JSONObject coordJSON = rootJSON.getJSONObject("coord");
+                // 緯度情報文字列を取得
+                latitude = coordJSON.getString("lat");
+                // 軽度情報文字列を取得
+                longitude = coordJSON.getString("log");
+                // 天気情報JSON配列オブジェクトを取得
+                JSONArray weatherJSONArray = rootJSON.getJSONArray("weather");
+                // 現在の天気情報JSONオブジェクトを取得
+                JSONObject weatherJSON = weatherJSONArray.getJSONObject(0);
+                // 現在の天気情報文字列を取得
+                weather = weatherJSON.getString("description");
+            }catch (JSONException ex) {
+                Log.e(DEBUC_TAG, "JSOn解析失敗", ex);
+            }
+
+            // 画面に表示する００天気文字列を生成
+            String telop = cityName + "の天気";
+            // 天気の詳細情報を表示する文字列を生成
+            String desc = "現在は" + weather + "です。\n緯度は" + latitude + "度で軽度は" + longitude + "度です";
+            // 天気情報を表示するTextViewを取得
+            TextView tvWeatherTelop = findViewById(R.id.tvWeatherTelop);
+            TextView tvWeatherDesc = findViewById(R.id.tvWeatherDesc);
+            // 天気情報を表示
+            tvWeatherTelop.setText(telop);
+            tvWeatherDesc.setText(desc);
         }
     }
 
